9ac0fe161c832cd5d9ae371aee8befb4e9868510
Michael Salerno <msalerno1993@gmail.com>
2023-12-10 13:50:02 -0500

    starting button component

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div>Button</div>
+    <div className='py-1 px-1 rounded-lg bg-gray-300 text-black text-lg items-center text-center'>
+      Contact Us!
+    </div>
   )
 }
 
