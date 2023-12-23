99d174cb42a34e39c0570c3daf1f858c6e90d282
Michael Salerno <msalerno1993@gmail.com>
2023-12-23 16:42:50 -0500

    footer started - layout complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./Pages/About"
 import Contact from "./Pages/Contact"
 import Careers from "./Pages/Careers"
 import Services from "./Pages/Services";
+import Footer from "./Components/Footer"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/careers" element={<Careers />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { FaInstagram } from "react-icons/fa"
+import { FaFacebook } from "react-icons/fa"
+import { FaGoogle } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='grid grid-cols-1 pt-5 pb-5 bg-black text-white'>
+      <div className='text-center'>
+        <li className='list-none font-logo text-4xl'>Jimmy's Barbershop LLC</li>
+      </div>
+      <div className='flex justify-center items-center space-x-2 pt-3'>
+      <button className='list-none text-3xl hover:text-gray-400'><FaInstagram /></button>
+      <button className='list-none text-3xl hover:text-gray-400'><FaFacebook /></button>
+      <button className='list-none text-3xl hover:text-gray-400'><FaGoogle /></button>
+      </div>
+    </div>
   )
 }
 
