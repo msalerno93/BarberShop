4a548706277dcac14233189f624043cb515a3db4
Michael Salerno <msalerno1993@gmail.com>
2023-12-24 13:26:40 -0500

    footer completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,11 +8,16 @@ const Footer = () => {
     <div className='grid grid-cols-1 pt-5 pb-5 bg-black text-white'>
       <div className='text-center'>
         <li className='list-none font-logo text-4xl'>Jimmy's Barbershop LLC</li>
+        <li className='list-none text-xl'>Established 1965</li>
       </div>
       <div className='flex justify-center items-center space-x-2 pt-3'>
       <button className='list-none text-3xl hover:text-gray-400'><FaInstagram /></button>
       <button className='list-none text-3xl hover:text-gray-400'><FaFacebook /></button>
       <button className='list-none text-3xl hover:text-gray-400'><FaGoogle /></button>
+      </div>
+      <div className='text-center pt-2'>
+        <div>Wesbite created & maintained by</div>
+        <a href='https://salernomichael-portfolio.netlify.app/' target='_blank' rel='noreferrer' className='text-lg hover:text-gray-400'>Salerno Inc</a>
       </div>
     </div>
   )
