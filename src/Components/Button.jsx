68c96aac8cb208180dd3a8c1c9d92ab309460a87
Michael Salerno <msalerno1993@gmail.com>
2023-12-24 14:04:34 -0500

    homepage/footer/navbar complete

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const Button = ({buttonName, pathName}) => {
   return (
-    <Link to={pathName} className='py-1 px-4 rounded-lg bg-black text-white font-bold text-lg items-center text-center'>
+    <Link to={pathName} className='py-1 px-4 rounded-lg bg-black text-white font-bold text-xl items-center text-center hover:text-gray-400'>
       {buttonName}
     </Link>
   )
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from '../Components/Button'
 
 const img =
   "https://thefader-res.cloudinary.com/private_images/w_2400,c_limit,f_auto,q_auto:best/40380002_WEB_gmcgws/levels-barbershop-new-york-ny.jpg";
@@ -9,25 +10,40 @@ const Homepage = () => {
   return (
     <div>
       <div className="pt-28 text-center w-full h-full items-center px-24 grid lg:grid-cols-2 md:grid-cols-1">
-        <img src={img} alt="homepageimg" className="w-full rounded-lg shadow-2xl shadow-black" />
-        <div className="sm:pt-10 md:pt-10 pt-5 px-10 text-2xl">
-          Welcome to Jimmy's Barbershop, where tradition meets style!
+        <img
+          src={img}
+          alt="homepageimg"
+          className="w-full rounded-lg shadow-2xl shadow-black"
+        />
+        <div className="sm:pt-10 md:pt-5 pt-5 px-10">
+          <div className="font-logo font-bold text-7xl">Jimmy's Barbershop</div>
+          {/* Welcome to Jimmy's Barbershop, where tradition meets style!
           Established in 1965, we take pride in being a family-owned and
           operated haven for all your grooming needs. With decades of
           experience, our skilled barbers have been dedicated to delivering
-          top-notch services that stand the test of time.
+          top-notch services that stand the test of time. */}
+          <div className="text-2xl">Established 1965</div>
+          <div className="pt-2 pb-2">
+            <div className="text-xl">56 125th Street</div>
+            <div className="text-xl">Brooklyn, NY 11350</div>
+            <div className="text-xl">555-555-5555</div>
+          </div>
+          <Button buttonName="Contact Us" pathName="/contact"/>
         </div>
       </div>
       <div className="py-10 text-center w-full h-full items-center px-24 grid lg:grid-cols-2 md:grid-cols-1">
         <div className="sm:pt-10 md:pt-10 px-5 pt-5 py-10 text-2xl">
-          Here we specialize in a variety of haircuts, ranging
-          from classic styles to contemporary trends. Our talented team is
-          well-versed in the art of beard trims, ensuring your facial hair is
-          groomed to perfection. Beyond haircuts and trims, we offer
-          personalized hair styling services to bring out your unique
-          personality and flair.
+          Here we specialize in a variety of haircuts, ranging from classic
+          styles to contemporary trends. Our talented team is well-versed in the
+          art of beard trims, ensuring your facial hair is groomed to
+          perfection. Beyond haircuts and trims, we offer personalized hair
+          styling services to bring out your unique personality and flair.
         </div>
-        <img src={img2} alt="homepageimg" className="w-full rounded-lg shadow-2xl shadow-black" />
+        <img
+          src={img2}
+          alt="homepageimg"
+          className="w-full rounded-lg shadow-2xl shadow-black"
+        />
       </div>
     </div>
   );
